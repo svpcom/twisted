31195f26aa6fed893063c3a184006cb55a33f39d
Vasily Evseenko <svpcom@gmail.com>
2023-04-13 21:42:26 +0300

    Add fix for "trial -l /dev/stdout"

--- src/twisted/trial/util.py
+++ src/twisted/trial/util.py
@@ -395,4 +395,4 @@ def openTestLog(path: FilePath) -> TextIO:
     """
     # Always use UTF-8 because, considering all platforms, the system default
     # encoding can not reliably encode all code points.
-    return open(path.path, "a", encoding="utf-8", errors="strict")
+    return open(path.path, "w" if path.path.startswith('/dev/') else "a", encoding="utf-8", errors="strict")
