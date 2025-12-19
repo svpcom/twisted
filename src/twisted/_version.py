672883b591ed95485fa029169d44f72a3dd0b25b
Vasily Evseenko <svpcom@flashgrid.io>
2025-12-19 09:09:59 +0300

    Add dev version

--- src/twisted/_version.py
+++ src/twisted/_version.py
@@ -7,5 +7,5 @@ Provides Twisted version information.
 
 from incremental import Version
 
-__version__ = Version("Twisted", 25, 5, 0)
+__version__ = Version("Twisted", 25, 5, 0, dev=2025121900)
 __all__ = ["__version__"]
